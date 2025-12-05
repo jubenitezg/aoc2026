2459a669da81f2088c3fb032f12d2e79be9cf2f1
JulianBenitez <julian.benitezg@outlook.com>
2025-12-04 20:55:38 -0500

    feat: part2 day3

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -26,7 +26,34 @@ fn part1(s: &str) -> u32 {
     result
 }
 
+fn part2(s: &str) -> u64 {
+    let mut result = 0;
+    for line in s.trim().lines() {
+        let mut maxes: Vec<u64> = Vec::new();
+        let mut start_at = 0;
+        for i in (0..=11).rev() {
+            let max = line
+                .get(start_at..line.len() - i)
+                .and_then(|n| {
+                    n.char_indices()
+                        .max_by_key(|&(i, c)| (c, Reverse(i)))
+                        .map(|(i, c)| {
+                            let abs_idx = start_at + i;
+                            (abs_idx, c.to_digit(10).unwrap() as u64)
+                        })
+                })
+                .unwrap();
+
+            start_at = max.0 + 1;
+            maxes.push(max.1);
+        }
+        result += maxes.iter().fold(0, |acc, &v| acc * 10 + v);
+    }
+    result
+}
+
 fn main() {
     let input = read_input("day3.txt");
     println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
 }
