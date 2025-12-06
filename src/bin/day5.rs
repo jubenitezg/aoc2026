fe2dd2107dc6364e9823e27f05dff7db88cc8544
JulianBenitez <julian.benitezg@outlook.com>
2025-12-05 23:56:01 -0500

    feat: day5 part2

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -32,7 +32,40 @@ fn part1(s: &str) -> u64 {
     fresh_ingredient
 }
 
+fn part2(s: &str) -> u64 {
+    let parts: Vec<&str> = s.trim().split_whitespace().collect();
+
+    let mut fresh_ids: Vec<(u64, u64)> = parts
+        .into_iter()
+        .filter(|&c| c.contains("-"))
+        .map(|l| {
+            let mut ranges = l.split('-');
+            let first: u64 = ranges.next().unwrap().parse().unwrap();
+            let last: u64 = ranges.next().unwrap().parse().unwrap();
+            (first, last)
+        })
+        .collect();
+
+    fresh_ids.sort_by_key(|&(a, _)| a);
+
+    let mut combined: Vec<(u64, u64)> = Vec::new();
+    for (a, b) in fresh_ids {
+        if let Some(last) = combined.last_mut() {
+            if a <= last.1 + 1 {
+                last.1 = last.1.max(b);
+            } else {
+                combined.push((a, b));
+            }
+        } else {
+            combined.push((a, b));
+        }
+    }
+
+    combined.iter().fold(0, |acc, &(a, b)| acc + (b - a) + 1)
+}
+
 fn main() {
     let input = read_input("day5.txt");
     println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
 }
