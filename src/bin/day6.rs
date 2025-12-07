eefb3fdc87fd7a4c20ab3c610034566b72e30b86
JulianBenitez <julian.benitezg@outlook.com>
2025-12-07 00:44:24 -0500

    feat: day6 part2

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -1,14 +1,5 @@
 use aoc2026::read_input;
 
-fn pretty_print(v: &Vec<Vec<&str>>) {
-    for row in v {
-        for c in row {
-            print!("{} ", c);
-        }
-        println!("");
-    }
-}
-
 fn part1(s: &str) -> u64 {
     let matrix: Vec<Vec<&str>> = s
         .trim()
@@ -40,7 +31,62 @@ fn part1(s: &str) -> u64 {
     result
 }
 
+fn chars_to_number(v: &[char]) -> u64 {
+    v.iter()
+        .filter_map(|c| c.to_digit(10))
+        .fold(0, |acc, v| acc * 10 + v as u64)
+}
+
+fn apply(acc: &[Vec<char>], op: &str) -> u64 {
+    let mut acc_mult = 1;
+    let mut acc_sum = 0;
+    for v in acc {
+        let n = chars_to_number(v);
+        match op {
+            "*" => acc_mult *= n,
+            "+" => acc_sum += n,
+            _ => panic!("help"),
+        };
+    }
+    if acc_mult > 1 {
+        return acc_mult + acc_sum;
+    }
+    acc_sum
+}
+
+fn part2(s: &str) -> u64 {
+    let mut lines: Vec<&str> = s.trim().lines().collect();
+    let ops: Vec<&str> = lines.pop().unwrap().split_whitespace().collect();
+
+    let rows = lines.len();
+    let cols = lines[0].len();
+    let mut result = 0;
+    let mut ops_idx = 0;
+
+    let mut acc: Vec<Vec<char>> = Vec::new();
+
+    for c in 0..cols {
+        let vertical: Vec<char> = (0..rows).map(|r| lines[r].as_bytes()[c] as char).collect();
+        let all_spaces = vertical.iter().all(|&c| c == ' ');
+        if all_spaces {
+            let op = ops[ops_idx];
+            result += apply(&acc, op);
+            acc.clear();
+            ops_idx += 1;
+        } else {
+            acc.push(vertical);
+        }
+    }
+    if !acc.is_empty() {
+        let op = ops[ops_idx];
+        result += apply(&acc, op);
+    }
+
+    result
+}
+
 fn main() {
     let input = read_input("day6.txt");
     println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,12 @@ pub fn read_input(input: &str) -> String {
     let base_path = Path::new("inputs");
     fs::read_to_string(base_path.join(input)).expect("Failed to read file")
 }
+
+pub fn pretty_print_matrix(v: &Vec<Vec<&str>>) {
+    for row in v {
+        for c in row {
+            print!("{} ", c);
+        }
+        println!("");
+    }
+}
